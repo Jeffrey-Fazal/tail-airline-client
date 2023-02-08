27f346217950baf08bbaf319976309e760e4d7d9
Jeffrey Fazal <jeffreyfazal@gmail.com>
2023-02-09 07:49:26 +1100

    added bootstrap support

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -1,5 +1,28 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 // import FlightsTable from '../../common/FlightsTable';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
+class NavbarBootStrap extends React.Component {
+    render() {
+        return (
+        <div>   
+            <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">Burning Airline</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/search">Search</Nav.Link>
+                    <Nav.Link href="/planes">Planes</Nav.Link>
+                    <Nav.Link href="/flights">Flights</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+        </div>
+        )
 
-// export default NavBar;
+    }
+}
+
+export default NavbarBootStrap;
