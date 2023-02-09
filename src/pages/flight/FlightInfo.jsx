d7c8dbd9b255bdf10cf54f28448ea2e20ea7c3c5
HueNguyen <nguyenhue.se@gmail.com>
2023-02-09 11:15:52 +1100

    working on flightinfo

--- src/pages/flight/FlightInfo.jsx
+++ src/pages/flight/FlightInfo.jsx
@@ -3,20 +3,19 @@ import axios from 'axios';
 
 import { FlightsTableHeader, FlightRow } from '../../common/FlightsTable';
 
-const FLIGHT_URL = `http://localhost:3001/flights/`
 class FlightInfo extends Component {
     constructor() {
         super();
         this.state = {
-            id: null,
+            flight_url: ''
         }
     }
 
     // fetch data from the server
     componentDidMount() {
         const fetchFlight = () => {
-            console.log('Fetching flight at', FLIGHT_URL);
-            axios.get(FLIGHT_URL).then((response) => {
+            console.log('Fetching flight at', this.state.flight_url);
+            axios.get(this.state.flight_url).then((response) => {
                 console.log(response.data);
                 return response.data;
             });
